a5b5414ccb0bc4f11724deecf1b6a3eb854bcca3
zjufisher <zjusher@zjufisherdeMacBook-Pro.local>
2018-12-01 17:06:20 -0800

    mem

---
+++ memory/mem_bandwidth.cpp
@@ -0,0 +1,32 @@
+//
+// Created by zjufisher on 12/1/18.
+//
+
+
+// memcpy
+// write, read
+
+int main() {
+
+}
+
+
+
+
+void testMemBandCopy() {
+
+}
+
+
+
+void testMemBandWrite() {
+
+
+}
+
+
+void testMemBandRead() {
+
+
+}
+
