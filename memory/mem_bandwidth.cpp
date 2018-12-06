15b425606ec55a9b844bfa68c3b0fd7d782d8d71
黄兆维 <huangzhaowei@huangzhaoweideMac-mini.local>
2018-12-05 16:01:55 -0800

    write

--- memory/mem_bandwidth.cpp
+++ memory/mem_bandwidth.cpp
@@ -11,12 +11,16 @@
 #include <stdio.h>
 #include "../util/util.cpp"
 
-#define ARR_SIZE 8388608
+#define ARR_SIZE 8388608 // 8M
+
+
 using namespace std;
 void testMemBandCopy();
+void testMemBandWrite();
 
 int main() {
     testMemBandCopy();
+    testMemBandWrite();
 }
 
 
@@ -26,7 +30,7 @@ void testMemBandCopy() {
 //    int src1[ARR_SIZE];
 //    int src2[ARR_SIZE];
 //    int dest[ARR_SIZE];
-    int* src1 = new int[ARR_SIZE];
+    int* src1 = new int[ARR_SIZE]; // 32MB
     int* src2 = new int[ARR_SIZE];
     int* dest = new int[ARR_SIZE];
 
@@ -50,7 +54,7 @@ void testMemBandCopy() {
     // total memory: 32K * 32M = 1024G
     double cycle_time = 1/(2.3*1000000000);
     double mem_total = 1024; // 1024G
-    cout << "total cpu cycles: " << total << endl;
+    cout << "total cpu cycles: " << total_cycle << endl;
     cout << "total time = " << mem_total / (total_cycle*cycle_time) << "G/s" << endl;
     delete[] src1;
     delete[] src2;
@@ -60,7 +64,40 @@ void testMemBandCopy() {
 
 
 void testMemBandWrite() {
+    int stride = ARR_SIZE / 8; 
+    int* src = new int[ARR_SIZE];
+    fill(src, src + ARR_SIZE, 0);
+    int pos = 0;
+    uint64_t start_tsc = rdtsc();
+    double total_tsc = 0;
+    uint64_t end_tsc = rdtsc();
+    for (int i = 0; i < 1024*1024; i++) {
+        start_tsc = rdtsc();
+        src[0] = 0;
+        src[16] = 0;
+        src[32] = 0;
+        src[48] = 0;
+        src[64] = 0;
+        src[80] = 0;
+        src[96] = 0;
+        src[112] = 0;
+        end_tsc = rdtsc();
+        // 64B * 8 = 512B
+        total_tsc += end_tsc - start_tsc;
+        for (int j = 0; j < 1000; j++) {
+            src[j+1024] = 0;
+        }
 
+
+    }
+    
+    // assume 4M cache 
+    // 16M * 128 = 2G
+
+    double cycle_time = 1 / (2.3*1000000000);
+    double mem_total = 0.5;
+    cout << "bandwidth write: " << mem_total / (total_tsc*cycle_time) << "G/s" << endl;
+    delete[] src;
 
 }
 
